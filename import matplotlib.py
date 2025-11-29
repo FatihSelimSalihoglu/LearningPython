83c4ceb31d39c42d9ddd5062f4f1e5f47c42a878
Fatih Selim Salihoglu <fatihsmail41@gmail.com>
2025-11-29 13:13:49 +0100

    Add files via upload
    My matplotlib trial .py file.

---
+++ import matplotlib.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as myplot1
+
+x = [2, 2.5, 3, 4, 5, 6]
+y = [3, 2.5, 1.5, 1, 2, 3]
+myplot1.title ('Mi Bombo')
+myplot1.xlabel ('trial')
+myplot1.ylabel ('error')
+myplot1.plot(x, y)
+myplot1.show()
+
+import matplotlib.pyplot as myplot2
+
+z = ([1, 2, 3])
+c = ([3, 4, 5])
+myplot2.title ('life is good')
+myplot2.zlabel ('life is maybe not so good')
+myplot2.clabel ('maybe good')
+myplot2.plot(z, c)
+myplot2.show()
